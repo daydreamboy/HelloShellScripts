0237251978e012a8a4ff155780c43ac6d27846af
daydreamboy <wesley4chen@gmail.com>
2018-05-15 21:57:06 +0800

    add script

--- 12_string_compare.sh
+++ 12_string_compare.sh
@@ -2,8 +2,27 @@
 
 str='mh_dylib'
 
+# Case 1: use `==`
 if [ ${str} == "mh_dylib" ]; then
-    echo "equal"
+    echo "equal (use '==')"
 else
-    echo "not equal"
+    echo "not equal (use '==')"
+fi
+
+# Case 2: use `=`
+if [ ${str} = "mh_dylib" ]; then
+    echo "equal (use '=')"
+else
+    echo "not equal (use '=')"
+fi
+
+# Case 3: regex match
+# @see https://stackoverflow.com/a/229606
+string='My long string'
+if [[ $string = *"My long"* ]]; then
+    echo "It's there! (use '=')"
+fi
+
+if [[ $string == *"My long"* ]]; then
+    echo "It's there! (use '==')"
 fi
