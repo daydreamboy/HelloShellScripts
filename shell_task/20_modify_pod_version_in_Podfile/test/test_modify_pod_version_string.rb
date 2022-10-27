ef5d98f3b29b42d721663fe57bce28fcf1c5b539
daydreamboy <wesley4chen@gmail.com>
2022-10-27 20:39:32 +0800

    shell_task: add code

--- shell_task/20_modify_pod_version_in_Podfile/test/test_modify_pod_version_string.rb
+++ shell_task/20_modify_pod_version_in_Podfile/test/test_modify_pod_version_string.rb
@@ -1,13 +1,12 @@
 #!/usr/bin/env ruby
 
-pod_name = 'SomePod1'
+pod_name = 'SomePod4'
 pod_version = '3.5.6'
 file_path = File.expand_path('../Podfile1.txt')
 
 command = "sh ../modify_pod_version_string.sh #{pod_name} #{pod_version} #{file_path}"
 puts "run command: #{command}"
-#`#{command}`
-system command
+`#{command}` # also use system command
 if $?.to_i != 0
   raise 'run shell failed'
 end
